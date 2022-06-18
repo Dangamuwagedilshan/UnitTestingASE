7981eff334dd0fd214408d5382186d53681f17d1
Dangamuwagedilshan <dangamuwage.dilshan@gmail.com>
2022-06-18 23:53:36 +0530

    print all the values of the simple class

---
+++ src/reflection/apis/Refelction01.java
@@ -0,0 +1,12 @@
+package reflection.apis;
+
+public class Refelction01 {
+
+	public static void main(String[] args) {
+		// get all 
+		Simple s = new Simple();
+		System.out.println("Output:"+s);
+
+	}
+
+}
--- src/reflection/apis/Simple.java
+++ src/reflection/apis/Simple.java
@@ -2,42 +2,62 @@ package reflection.apis;
 
 public class Simple {
 
-  public int a = 10;
-  public int b = 20;
-  
-  public Simple() {
-  }
-  
-  public Simple(int a, int b) {
-  this.a = a;
-  this.b = b;
-  }
+	public String firstName = "Adeesha";
+	public String lastName = "Dangamuwage";
+	public String fullName;
+	private int age = 24;
 
-  public void squareA() {
-    this.a *= this.a;
-  }
-  
-  public void squareB() {
-    this.b *= this.b;
-  }
-  
-  public int getA() {
-    return a;
-  }
+	public Simple() {
+	}
 
-  public int getB() {
-    return b;
-  }
-  
-  public void setA(int a) {
-    this.a = a;
-  }
-  
-  public void setB(int b) {
-    this.b = b;
-  }
-  
-  public String toString() {
-    return String.format("(a:%d, b:%d)", a, b);
-  }
+	public Simple(String firstName, String lastName, String fullName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.fullName = fullName;
+		this.age = age;
+	}
+
+	public void fullName() {
+		this.fullName = firstName + " " + lastName;
+	}
+
+	public void age5YearsBack() {
+		this.age -= 5;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String toString() {
+		return String.format("(firstName:%s, lastName:%s, Age:%d)", firstName, lastName,age);
+	}
 }
