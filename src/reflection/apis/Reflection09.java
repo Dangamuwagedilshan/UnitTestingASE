66360605585a88550480fa53cfe3ac52c2608ee0
Dangamuwagedilshan <dangamuwage.dilshan@gmail.com>
2022-06-20 16:24:06 +0530

    printing method names and the method lengths and also the names of the class

---
+++ src/reflection/apis/Reflection09.java
@@ -0,0 +1,28 @@
+package reflection.apis;
+
+import java.lang.reflect.Method;
+
+public class Reflection09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Simple s = new Simple();
+	    Method[] methods = s.getClass().getMethods();
+	    System.out.printf("There are %d methods\n", methods.length);
+	    
+	    for (Method m : methods) {
+	      
+	      System.out.printf("method name=%s type=%s parameters =",
+	        m.getName(), m.getReturnType());
+	      Class[] types = m.getParameterTypes();
+	        
+	      for(Class c : types) {
+	        System.out.print(c.getName() + ""); 
+	      }
+	      
+	      System.out.println();
+	    }
+	    
+	}
+
+}
