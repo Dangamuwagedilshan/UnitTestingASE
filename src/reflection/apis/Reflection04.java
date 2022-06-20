d7c5bb27c13e68e2d933fa1f97c826193a0e29c4
Dangamuwagedilshan <dangamuwage.dilshan@gmail.com>
2022-06-20 16:08:27 +0530

    Printing each of fields name type and value.

--- src/reflection/apis/Reflection04.java
+++ src/reflection/apis/Reflection04.java
@@ -4,13 +4,21 @@ import java.lang.reflect.Field;
 
 public class Reflection04 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws  NoSuchFieldException,
+    SecurityException,
+    IllegalArgumentException,
+    IllegalAccessException {
 		// TODO Auto-generated method stub
-		Simple s = new Simple();
-		Field[] fields = s.getClass().getFields();
+		Simple sm = new Simple();
+		Field[] fields = sm.getClass().getFields();
+		
 		System.out.printf("There are %d fields\n", fields.length);
+	
+		
+		
 		for(Field f : fields) {
-	        System.out.printf("field name=%s type=%s value=%s\n", f.getName(), f.getType(),f.getGenericType());
+			
+	        System.out.printf("field name=%s type=%s value=%.2f\n" ,f.getName(), f.getType(),f.getDouble(sm));
 	    }
 	}
 
