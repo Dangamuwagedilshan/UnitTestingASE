827619046497d3a5a5c1985ed26756b9f0e0498e
Dangamuwagedilshan <dangamuwage.dilshan@gmail.com>
2022-06-20 16:12:48 +0530

    Committing To show case that members with private modifiers cannot be accessed

--- src/reflection/apis/Reflection05.java
+++ src/reflection/apis/Reflection05.java
@@ -13,7 +13,7 @@ public class Reflection05 {
 		    System.out.printf("There are %d fields\n", fields.length);
 		    
 		 for(Field f : fields) {
-		      System.out.printf("field name=%s type=%s value=%s\n", 
+		      System.out.printf("field name =%s type =%s value =%s\n", 
 		          f.getName(), f.getType(), f.getName().toString());
 		      
 		 }
