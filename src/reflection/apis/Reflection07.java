c4546c409fbabe74c4c4e4c1345344ec368bb5d5
Dangamuwagedilshan <dangamuwage.dilshan@gmail.com>
2022-06-20 16:21:09 +0530

    Setting the accessibility flag to true to show case the data integrity is not as solid. setAccessible(true) to get access to the values of the class to obtain the name and value of the private member.

---
+++ src/reflection/apis/Reflection07.java
@@ -0,0 +1,24 @@
+package reflection.apis;
+
+import java.lang.reflect.Field;
+
+
+public class Reflection07 {
+
+	public static void main(String[] args) throws  NoSuchFieldException,
+    SecurityException,
+    IllegalArgumentException,
+    IllegalAccessException   {
+		// TODO Auto-generated method stub
+		Simple s = new Simple();
+	    Field[] fields = s.getClass().getDeclaredFields();
+	    System.out.printf("There are %d fields\n", fields.length);
+	    
+	    for (Field f : fields) {
+	      f.setAccessible(true);
+	      System.out.printf("field name=%s type=%s value=%.2f\n", f.getName(),
+	        f.getType(),f.getDouble(s));
+	    }
+	}
+
+}
